0d26af3153d07f47d5dfd34f0834a80b89dff42f
Xzhi <cx_zhi@qq.com>
2026-09-07 03:20:20 +0800

    fix: 流转时间useMemo遗漏依赖导致不渲染

--- src/components/Item/MetadataTable.tsx
+++ src/components/Item/MetadataTable.tsx
@@ -142,6 +142,9 @@ export function ItemMetadata({
     completedAt,
     shouldShowCompletedTime,
     cardCompletedTimeFormat,
+    shouldShowFlowTime,
+    lastFlow,
+    flowTimeFormat,
   ]);
 
   const order = useMemo(() => {
